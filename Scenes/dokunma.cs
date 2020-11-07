e1989a526b5db4f8c2214ea5eb4af7404b37e323
Hüseyin Can Şimşek <51913434+HusoBey21@users.noreply.github.com>
2020-11-07 23:47:26 +0300

    Add files via upload

---
+++ Scenes/dokunma.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dokunma : MonoBehaviour
+{
+    public List<Transform> donusum;
+    solaYonel renk;
+    Transform fish;
+    public void Start()
+    {
+        donusum = new List<Transform>(); //Bunu ilklendirdik.
+        renk = GetComponent<solaYonel>();
+        if (fish != null)
+        {
+            fish = GetComponent<temas>().balik;
+        }
+    }
+     void FixedUpdate()
+    {
+        renk.Uretim();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+       
+        if(collider.gameObject.transform.tag==fish.tag)
+        {
+            
+            Debug.Log("Vurun");
+            donusum.Add(fish);
+        }
+        
+        
+    }
+}
